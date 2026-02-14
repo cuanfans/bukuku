1877e4fd0ed46a89941ce9402b74dd04f0ecf51e
cuanfans <cuanfans.com@gmail.com>
2026-02-14 13:57:06 +0700

    Create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+export const MODAL_TYPES = [
+  { value: 'fastpay_fa420355', label: 'Fastpay FA420355' },
+  { value: 'fastpay_fa851085', label: 'Fastpay FA851085' },
+  { value: 'modal_kas', label: 'Modal Kas' },
+  { value: 'karang_sari', label: 'Karang Sari' },
+  { value: 'payfazz', label: 'Payfazz' },
+  { value: 'buku_warung', label: 'Buku Warung' },
+  { value: 'mmbc', label: 'MMBC' },
+  { value: 'posfin', label: 'Posfin' },
+  { value: 'pospay', label: 'Pospay' },
+  { value: 'bsi_agen', label: 'BSI Agen' },
+  { value: 'bni_agen', label: 'BNI Agen' },
+  { value: 'brilink', label: 'BRILink' },
+  { value: 'mandiri_agen', label: 'Mandiri Agen' }
+];
